60dbb178279f79197ed3266aa9c1582257393223
Clodoaldo Fávero <clodoaldofavero@gmail.com>
2018-10-25 00:58:04 +0000

    Salva em json corrigido

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import pickle
 import os
 import operator
 import configparser
-import json
+import tweet
+import jsonpickle
+
+
 
 # Sobre url de busca no twitter
 # https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets.html
@@ -11,81 +14,94 @@ import json
 # https://developer.twitter.com/en/docs
 
 
-
 # Carrega as chaves de acesso
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# Salva os dados para acesso
 consumer_key = config['CONSUMER KEY']['consumer_key']
 consumer_secret = config['CONSUMER KEY']['consumer_secret']
 access_token = config['ACCESS TOKEN']['access_token']
 access_token_secret = config['ACCESS TOKEN']['access_token_secret']
 
+
+# Acessa a api do Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+
+# Define os termos a serem ignorados no processamento dos textos
 irrelevantes = ['e', 'ou', 'para', 'em', 'na', 'no', 'lá', 'quando', 'que', 'a', 'o', 'mas', 'mais', 'porque', 'por',
                 'me', 'de', 'da', 'do']
 
-id_list = {}
 
 # Retorna 100 tweets
 tweets = tweepy.Cursor(api.search, q="ps4 playstation4 -filter:retweets", rpp=100, result_type='recent',
-                       tweet_mode='extended', exclude_replies=True).items(3)
+                       tweet_mode='extended', exclude_replies=True).items(100)
 
-# Grava cada tweet em uma lista de json
+
+ 
+id_list = []
 lista_tweets = []
-tweet_json = {}
-for tweet in tweets:
-    id_list[tweet.id_str] = tweet.full_text
-    tweet_json['tweet_id'] = tweet.id_str
-    tweet_json['user_id'] = tweet.user.id_str
-    tweet_json['text'] = tweet.full_text
-
-    # Percorre as hashtags do tweet, adicionando cada uma à lista
-    hashtags_list = []
-    for hashtag in tweet.entities['hashtags']:
-        hashtags_list.append(hashtag['text'])
-    # Adiciona a lista no dict tweet_json
-    tweet_json['hashtags'] = hashtags_list
-    # Adiciona o tweet_json à lista de objetos json
-    lista_tweets.append(tweet_json)
-
-lista_json = {"tweets": lista_tweets}
-
-# Serializa os objetos json e salva em um arquivo
-with open('tweets.json', 'w+') as json_file:
-    json.dump(lista_json, json_file)
+requisicoes = 1
 
 
-
-exit()
-
-
-# Loop para pegar tweets mais antigos
-i = 1
+# Salva os dados dos tweets, e continua até o usuário interromper
 while True:
-    # Pega 100 tweets
+    
+    for t in tweets:
+        print(t.id)
+        id_list.append(t.id)
+        
+
+        # Percorre as hashtags do tweet, adicionando cada uma à lista
+        hashtags_list = []
+        for hashtag in t.entities['hashtags']:
+            hashtags_list.append(hashtag['text'])
+            
+        # Cria um objeto tweet
+        current_tweet = tweet.Tweet(t.id_str, t.full_text, hashtags_list, t.user.id_str)
+        
+        
+        
+        # Adiciona o objeto tweet 
+        lista_tweets.append(current_tweet)
+
+    
+    print('Requisicao: {requisicoes}'.format(requisicoes = requisicoes))
+    #opcao = input('Deseja continuar (Y/N)?')
+    requisicoes += 1
+    #if opcao.upper() == 'N':
+    #    break
+    
+    
+    if requisicoes > 10000:
+        break
+    # Salva o menor id dos tweets (mais antigo)
+    max_id = min(id_list)
+    # Pega os próximos 100 tweets com id menor que o id mínimo da requisição anterior
     tweets = tweepy.Cursor(api.search, q="ps4 playstation4 -filter:retweets", rpp=100, result_type='recent',
                            tweet_mode='extended', max_id=max_id - 1, exclude_replies=True).items(100)
+    
+    
+    
+    
 
-    # Salva o texto
-    for tweet in tweets:
-        id_list[tweet.id_str] = tweet.full_text
-    # Salva o id do tweet mais antigo
-    max_id = min(id_list.keys())
-    i += 1
-    # 100 + 50 * 100 = 5100 tweets
-    if i > 50:
-        break
+# Serializa os objetos json e salva em um arquivo
+with open('tweets.json', 'w', encoding='utf-8') as json_file:
+    json_file.write(jsonpickle.encode(lista_tweets))
 
-max_id = min(id_list.keys())
-print(len(id_list))
-print(max_id)
 
-# Salva os tweets em um arquivo texto
-with open('tweets.txt', 'w', encoding="utf-8") as file:
-    for key, value in id_list.items():
-        file.write(key + ',' + value + '\n')
-        # print(len(value))
+
+
+
+
+print('Quantidade de tweets salvos: {qtde_tweets}: '.format(qtde_tweets = len(id_list)))
+    
+
+    
+    
+
+
+
---
+++ tweet.py
@@ -0,0 +1,9 @@
+class Tweet():
+    
+    def __init__(self, tweet_id_str, full_text, hashtags, user_id):
+        
+        self.tweet_id_str = tweet_id_str
+        self.full_text = full_text
+        self.hashtags = hashtags
+        self.user_id = user_id
+        
