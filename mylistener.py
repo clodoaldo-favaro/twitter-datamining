996b0ba89f650bb1a7e74f7db3bdd41655215683
Clodoaldo Favero <clodoaldofavero@gmail.com>
2018-10-26 21:32:03 -0300

    Mostra contagem e tamanho do tweet.

--- mylistener.py
+++ mylistener.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
-
+import sys
+glob_tweet_count = 0
 class Listener(tweepy.StreamListener):
 
     def on_status(self, status):
@@ -8,8 +9,12 @@ class Listener(tweepy.StreamListener):
         try:
             with open('tweets.json', 'a') as f:
                 if (not status._json['retweeted']) and (not status._json['text'].startswith('RT @')):
+                    global glob_tweet_count
+                    glob_tweet_count += 1
                     data = json.dumps(status._json)
                     f.write(data + '\n')
+                    print("Tweet: {}".format(glob_tweet_count))
+                    print(sys.getsizeof(status._json))
         except BaseException as e:
             print('Error on_data: {error_message}'.format(error_message=e))
 
