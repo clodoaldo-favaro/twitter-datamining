b0b064ffef1440eefe6351983891f165cc77a846
Clodoaldo Favero <clodoaldofavero@gmail.com>
2018-10-27 22:09:44 -0300

    Lê os tweets no arquivo e conta o número de vezes que um jogo é mencionado.

--- processa_tweets.py
+++ processa_tweets.py
@@ -2,6 +2,11 @@ import json
 
 
 
+lista_jogos = ['WITCHER 3', 'METAL GEAR SOLID V', 'RED DEAD REDEMPTION 2', 'SPIDER-MAN', 'SHADOW OF THE TOMB RAIDER'
+               'GOD OF WAR', 'FORTNITE', 'GTA V', 'FIFA 19', 'HORIZON ZERO DAWN', "ASSASSIN'S CREED ODYSSEY", 'SOUL CALIBUR VI',
+               'CALL OF DUTY: BLACK OPS 4', 'PES 2019', 'MADDEN NFL 19', 'DETROIT: BECOME HUMAN',
+               'FALLOUT SHELTER', "ASSASSIN'S CREED BROTHERHOOD", 'THE ELDER SCROLLS ONLINE']
+contador_mencoes_jogos = {}
 
 with open('tweets.json', 'r') as f:
     for line in f:
@@ -10,8 +15,18 @@ with open('tweets.json', 'r') as f:
             texto = tweet['extended_tweet']['full_text']
         else:
             texto = tweet['text']
-        termos = texto.split()
-        for termo in termos:
-            if len(termo) > 3 and termo.istitle():
-                print(termo) 
 
+        texto = texto.upper()
+
+        jogos_lidos = []
+        for jogo in lista_jogos:
+            if (jogo in texto) and (jogo not in jogos_lidos):
+                jogos_lidos.append(jogo)
+                if jogo in contador_mencoes_jogos:
+                    contador_mencoes_jogos[jogo] += 1
+                else:
+                    contador_mencoes_jogos[jogo] = 1
+
+
+for key, value in contador_mencoes_jogos.items():
+    print(key, ':', value)
