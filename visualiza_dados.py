3cca3f3ab378e34ffe2214ba099aa24227de6275
Clodoaldo Favero <clodoaldofavero@gmail.com>
2018-10-30 15:14:28 -0300

    Visualiza frequência de jogos mencionandos em barras verticais.

---
+++ visualiza_dados.py
@@ -0,0 +1,41 @@
+import pymongo
+import configparser
+import pprint
+import matplotlib.pyplot as plt
+
+glob_tweet_count = 0
+
+# Carrega senha para acesso ao banco de dados
+config = configparser.ConfigParser()
+config.read('config.ini')
+password = config['MONGODB']['password']
+
+
+# Cria conexao
+client = pymongo.MongoClient("mongodb+srv://clodo:" + password + "@orgarq-bcluw.mongodb.net/test?retryWrites=true")
+
+# Acessa a base de dados (ou cria ela)
+db = client.tweets
+
+
+
+
+lista_jogos = ['WITCHER 3', 'METAL GEAR SOLID V', 'RED DEAD REDEMPTION 2', 'SPIDER-MAN', 'SHADOW OF THE TOMB RAIDER',
+               'GOD OF WAR', 'FORTNITE', 'GTA V', 'FIFA 19', 'HORIZON ZERO DAWN', "ASSASSIN'S CREED ODYSSEY", 'SOUL CALIBUR VI',
+               'CALL OF DUTY: BLACK OPS 4', 'PES 2019', 'MADDEN NFL 19', 'DETROIT: BECOME HUMAN',
+               'FALLOUT SHELTER', "ASSASSIN'S CREED BROTHERHOOD", 'THE ELDER SCROLLS ONLINE']
+
+
+
+frequencia_jogo = {}
+for jogo in lista_jogos:
+    frequencia_jogo[jogo] = db.tweets_resumidos.count_documents({'text': {'$regex': jogo, '$options': 'i'}})
+
+pprint.pprint(frequencia_jogo)
+
+plt.bar(range(len(frequencia_jogo)),frequencia_jogo.values(), align='center')
+plt.xticks(range(len(frequencia_jogo)), list(frequencia_jogo.keys()), rotation='vertical')
+
+
+plt.tight_layout()
+plt.show()
