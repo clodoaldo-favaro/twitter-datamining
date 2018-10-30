c9cf5049fe689fb76a03fd0f353f7a6ee6fbe326
Clodoaldo Favero <clodoaldofavero@gmail.com>
2018-10-30 09:56:41 -0300

    Conta o número de vezes que cada jogo aparece no campo texto da base de dados.

---
+++ consultas.py
@@ -0,0 +1,39 @@
+import tweepy
+import pymongo
+import json
+import configparser
+import pprint
+
+glob_tweet_count = 0
+
+# Carrega senha para acesso ao banco de dados
+config = configparser.ConfigParser()
+config.read('config.ini')
+password = config['MONGODB']['password']
+
+
+# Cria conexao
+client = pymongo.MongoClient("mongodb+srv://clodo:" + password + "@orgarq-bcluw.mongodb.net/test?retryWrites=true")
+
+# Acessa a base de dados (ou cria ela)
+db = client.tweets
+
+
+
+
+lista_jogos = ['WITCHER 3', 'METAL GEAR SOLID V', 'RED DEAD REDEMPTION 2', 'SPIDER-MAN', 'SHADOW OF THE TOMB RAIDER',
+               'GOD OF WAR', 'FORTNITE', 'GTA V', 'FIFA 19', 'HORIZON ZERO DAWN', "ASSASSIN'S CREED ODYSSEY", 'SOUL CALIBUR VI',
+               'CALL OF DUTY: BLACK OPS 4', 'PES 2019', 'MADDEN NFL 19', 'DETROIT: BECOME HUMAN',
+               'FALLOUT SHELTER', "ASSASSIN'S CREED BROTHERHOOD", 'THE ELDER SCROLLS ONLINE']
+
+
+
+frequencia_jogo = {}
+for jogo in lista_jogos:
+    frequencia_jogo[jogo] = db.tweets_resumidos.count_documents({'text': {'$regex': jogo, '$options': 'i'}})
+
+pprint.pprint(frequencia_jogo)
+
+
+
+
